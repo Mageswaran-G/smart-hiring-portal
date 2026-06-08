14dab9ea6d6c6760eda84c4be729b8e9c6f8c886
Mageswaran-G <mageswaran.g@outlook.com>
2026-06-08 12:19:09 +0530

    chore: remove temp debug scripts from repo

--- backend/verifypass.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const User = require('./models/User');
-  const user = await User.findOne({ email: 'candidate@test.com' }).select('+password');
-  const match = await bcrypt.compare('Test1234', user.password);
-  console.log('Password match:', match);
-  process.exit(0);
-}).catch(e => { console.log('Error:', e.message); process.exit(1); });
