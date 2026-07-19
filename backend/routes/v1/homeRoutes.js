acf847cf0ddab6c5c538d05dce241e8de65b14e9
Mageswaran-G <mageswaran.g@outlook.com>
2026-07-19 12:14:15 +0530

    chore: remove unused home routes

--- backend/routes/v1/homeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const homeController = require("../../controllers/public/homeController");
-
-// Public Home API
-router.get("/", homeController.getHomeData);
-
-module.exports = router;
